ad0b97e2f13eba8f44a04ae1805ed3f3c13768e1
shellylin <shellylin@hitrust.com.tw>
2022-03-20 19:19:15 +0800

    update experience

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,52 +1,54 @@
 
 <template>
- <svg class="absolu9te" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 310">
-  <path fill="#fdbc66" fill-opacity="1" d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-</svg>
-<div style="background-color:#fdbc66;border-bottom-left-radius: 50px;border-bottom-right-radius: 50px;margin-top:-2%">
-  <div class="container flex flex-col mx-auto sm:px-12 sm:flex-row md:flex-row">
-  <section class="text-gray-600 body-font">
-  <div class="container px-5 pb-24 mx-auto">
-    <h1 class="mb-4 text-3xl leading-none text-center lg:text-4xl " style="position: relative;">
-      <strong class="text-transparent text-[white] text-8xl iphoneX:text-[4rem]">Skill</strong>
-    </h1>
-    <div class="flex flex-wrap -ml-4 -ml-8 md:justify-center">
-      <div class="px-4 pt-8 lg:w-1/3">
-        <div class="flex items-start h-full">
-          <div class="flex-grow pl-6 text-center">
-            <span class="inline-flex items-center justify-center text-[24px] w-64 h-12 mb-4 text-[#0369a1] bg-[#eff6ff] rounded-md">Web/Layout</span>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 熟悉 Boostrap, Tailwind 等 UI Library, 並能打造 RWD 網站</p>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 使用 Figma 做UI畫面經驗</p>
+  <svg class="absolu9te" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 310">
+    <path fill="#fdbc66" fill-opacity="1"
+      d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+    </path>
+  </svg>
+  <div style="background-color:#fdbc66;border-bottom-left-radius: 50px;border-bottom-right-radius: 50px;margin-top:-2%">
+    <div class="container flex flex-col mx-auto sm:px-12 sm:flex-row md:flex-row">
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 pb-24 mx-auto">
+          <h1 class="mb-4 text-3xl leading-none text-center lg:text-4xl " style="position: relative;">
+            <strong class="text-transparent text-[white] text-8xl iphoneX:text-[4rem]">Skill</strong>
+          </h1>
+          <div class="flex flex-wrap -ml-4 -ml-8 md:justify-center">
+            <div class="px-4 pt-8 lg:w-1/3">
+              <div class="flex items-start h-full">
+                <div class="flex-grow pl-6 text-center">
+                  <span class="inline-flex items-center justify-center text-[24px] w-64 h-12 mb-4 text-[#0369a1] bg-[#eff6ff] rounded-md">Web/Layout</span>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 熟悉 Boostrap, Tailwind 等 UI Library, 並能打造 RWD 網站</p>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 使用 Figma 做UI畫面經驗</p>
+                </div>
+              </div>
+            </div>
+            <div class="px-4 pt-8 lg:w-1/3">
+              <div class="flex items-start h-full">
+                <div class="flex-grow pl-6 text-center">
+                  <span class="inline-flex items-center justify-center text-[24px] w-64 h-12 mb-4 text-[#0369a1] bg-[#eff6ff] rounded-md">React/Anglar/Vue</span>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 有 ES6 開發經驗</p>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 有 Angular 一年內開發經驗</p>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 了解 React Hook 特性, 避免不必要的畫面渲染</p>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 會使用 React Class Component</p>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 會使用 Vue 基礎語法、生命週期</p>
+                </div>
+              </div>
+            </div>
+            <div class="px-4 pt-8 lg:w-1/3">
+              <div class="flex items-start h-full">
+                <div class="flex-grow pl-6 text-center">
+                  <span class="inline-flex items-center justify-center text-[24px] w-64 h-12 mb-4 text-[#0369a1] bg-[#eff6ff] rounded-md">Git</span>
+                  <p class="text-base leading-relaxed text-left text-gray-600">- 熟悉 Sourcetree 版本控制介面, 與同事協作使用經驗</p>
+                  <p class="text-base leading-relaxed text-left text-gray-600 ">- 了解 Git Flow 工作流程</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="px-4 pt-8 lg:w-1/3">
-        <div class="flex items-start h-full">
-          <div class="flex-grow pl-6 text-center">
-            <span class="inline-flex items-center justify-center text-[24px] w-64 h-12 mb-4 text-[#0369a1] bg-[#eff6ff] rounded-md">React/Anglar/Vue</span>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 有 ES6、TypeScript 開發經驗</p>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 有 Angular 一年內開發經驗</p>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 了解 React Hook 特性, 避免不必要的畫面渲染</p>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 會使用 React Class Component</p>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 會使用 Vue 基礎語法、生命週期</p>
-          </div>
-        </div>
-      </div>
-       <div class="px-4 pt-8 lg:w-1/3">
-        <div class="flex items-start h-full">
-          <div class="flex-grow pl-6 text-center">
-            <span class="inline-flex items-center justify-center text-[24px] w-64 h-12 mb-4 text-[#0369a1] bg-[#eff6ff] rounded-md">Git</span>
-            <p class="text-base leading-relaxed text-left text-gray-600">- 熟悉 Sourcetree 版本控制介面, 與同事協作使用經驗</p>
-            <p class="text-base leading-relaxed text-left text-gray-600 ">- 了解 Git Flow 工作流程</p>
-          </div>
-        </div>
-      </div>
+      </section>
+
     </div>
   </div>
-</section>
-  
-  </div>
-</div>
   <!-- </div> -->
 </template>
 <script>
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -4,7 +4,7 @@
     <div class="container flex flex-col-reverse pr-4 mx-auto ml-4 iphoneX:mt-[4%]">
       <section class="text-gray-600 body-font iphoneX:mt-[2%]">
         <p class="text-xl font-bold leading-relaxed text-center">網際威信股份有限公司 前端工程師</p>
-        <p class="italic font-light leading-relaxed text-center">2021/08~現今</p>
+        <p class="italic font-light leading-relaxed text-center">2020/08~現今</p>
         <div class="px-5 py-16 mx-auto " style="margin-top:-4%">
           <div class="flex flex-col items-center pb-10 mx-auto mb-10 border-b border-gray-200 sm:flex-row">
             <div class="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 sm:w-32 sm:h-32 sm:mr-10">
@@ -17,7 +17,7 @@
               <span class="w-4 h-4 p-2 mb-1 mr-4 text-[0.5rem] text-white bg-blue-500 rounded-full">#CSS</span>
               <span class="w-4 h-4 p-2 mb-1 mr-4 text-[0.5rem] text-white bg-blue-500 rounded-full">#RWD</span>
               <div class="mt-4 ">
-                <p class="text-base leading-relaxed iphoneX:text-left">- 使用 Angular.js、TypeScript 提供企業用戶交易</p>
+                <p class="text-base leading-relaxed iphoneX:text-left">- 使用 Angular.js 提供企業用戶交易</p>
                 <p class="text-base leading-relaxed iphoneX:text-left">- 使用 Angular 套件 html2canvas 將PDF進行切版並預覽</p>
                 <p class="text-base leading-relaxed iphoneX:text-left">- 使用 chart.js 製作匯率趨勢圖並客製化漸層樣式</p>
                 <p class="text-base leading-relaxed iphoneX:text-left">- 有使用 IOS、Andriod 包版經驗</p>
@@ -37,7 +37,7 @@
                 <div class="iphoneX:h-[2.5rem]"><span class="w-4 h-4 p-2 mb-1 mr-4 text-[0.5rem] text-white bg-blue-500 rounded-full">#CSS</span></div>
               </div>
               <div class="mt-4 iphoneX:mt-0">
-                <p class="text-base leading-relaxed iphoneX:text-left">- 將前端程式碼進行優化、調整進而符合各瀏覽器</p>
+                <p class="text-base leading-relaxed iphoneX:text-left">- 將程式碼兼容各瀏覽器</p>
                 <p class="text-base leading-relaxed iphoneX:text-left">- 使用Jquery進行功能製作</p>
                 <p class="text-base leading-relaxed iphoneX:text-left">- 使用 Figma 設計富邦香港轉數快交易設定收款人、線上表單申請畫面</p>
               </div>
